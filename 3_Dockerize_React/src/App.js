7667defd2f149d47f2b9b256011197dc1633d7a0
Vinayak Arora <161114357+vinayak746@users.noreply.github.com>
2025-11-06 11:54:33 +0530

    Create App.js for Dockerized React application

---
+++ 3_Dockerize_React/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dockerized React App</h1>
+        <p>Multi-stage Docker build demonstration</p>
+        <p>Serving with Nginx</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
